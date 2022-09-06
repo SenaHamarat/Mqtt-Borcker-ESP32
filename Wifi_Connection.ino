fc8bb1a59e4b5d2404bf21030b9287bc56fd99b3
Sena Hamarat <sena.hamarat99@hotmail.com>
2022-09-06 11:33:15 +0200

    now working

--- Wifi_Connection.ino
+++ Wifi_Connection.ino
@@ -1,23 +1,69 @@
+// Summer School 2022
+// ESP32 test code for MQTT protocol using
+// a free MQTT broker by emqx
+// Gerardo Marx
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char *ssid = "KNUET-Gast";
-const char *password = "Knuet201";
+// WiFi configuration
+const char *ssid = "KNUET-Gast"; // WiFi name
+const char *password = "Knuet201";  // WiFi password
 WiFiClient espClient;
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.begin (ssid, password);
+// mqtt brocker:
+const char *mqttBrocker = "139.13.210.100";
+const char *topic = "esp32/test";
+const char *mqttUsername = "";
+const char *mqttPassword = "";
+const int mqttPort = 1883;
+PubSubClient client(espClient);
 
-  while (WiFi.status() != WL_CONNECTED)
-  { 
-   delay(500);
-   Serial.print("Connecting to ");
-   Serial.println(ssid);
+
+void setup(){
+  
+  //serial communication
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print("Connecting to ");
+    Serial.println(ssid); 
   }
-  Serial.println("Connection done. ");
+  Serial.println("Connection done.");
+  
+  //connecting to a mqtt brocker:
+  client.setServer(mqttBrocker, mqttPort);
+  client.setCallback(callback);
+  while(!client.connected()){
+    String clientID = "esp32-gmarx-";
+    clientID += String(WiFi.macAddress());
+    Serial.printf("The %s tries to connect to mqtt borcker...\n",clientID.c_str());
+    if(client.connect(clientID.c_str(), mqttUsername, mqttPassword)){
+      Serial.println("mqtt brocker connected");
+    }
+    else {
+      Serial.print("mqtt connection failed");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
+  
+  //once connected publish and suscribe:
+  client.publish(topic, "Hi EMQX broker I'm a ESP32 :)");
+  client.subscribe(topic);
 }
 
-void loop() {
-  //client.loop();
+void loop(){
+  client.loop();
+}
+
+void callback(char *topic, byte *payload, unsigned int length){
+  Serial.print("Message recived in topic: ");
+  Serial.println(topic);
+  Serial.print("The message is: ");
+  for(int i=0;i<length; i++){
+    Serial.print((char) payload[i]);
+  }
+  Serial.println();
+  Serial.println("-+-+-+End+-+-+-+");
 }
